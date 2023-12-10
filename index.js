b10e906da1398d9c53ad67b73b01d0ece1080c99
TJonCanon <taylor.joyner93@me.com>
2023-12-10 16:14:12 -0600

    api implemented but not working yet

--- index.js
+++ index.js
@@ -29,3 +29,49 @@ checkDB()
             consle.log("Database exists")
         }
         })
+        .then(() => {
+            const app = express();
+            const port = 5000;
+
+            const connection = createDBConnect();
+
+            app.get("/api/episodes/search", async (req, res) => {
+                const { month, subject, color, matchType } = req.query;
+
+                let filters = [];
+                if (month) filters.push(`month = ${connection.esacpe(month)}`);
+                if (subject) filters.push(`s.subject_name = ${connection.escape(subject)}`);
+                if (color) filters.push(`c.color_name = ${connection.escape(color)}`);
+
+                let filterString = filters.join(
+                    matchType.toUpperCase() === "ALL" ? " AND " : " OR "
+                  );
+
+                  let sql = `
+                  SELECT e.episode_title
+                  FROM Episodes e 
+                  LEFT JOIN Episode_Subject es ON e.episode_id = es.Episode_Id
+                  LEFT JOIN Subjects s ON es.Subject_Id = s.Subject_Id
+                  LEFT JOIN Episode_Color ec ON e.episode_id = ec.Episode_Id
+                  LEFT JOIN Colors c ON ec.Color_Id = c.Color_Id
+                `;
+                if (filterString) sql += `WHERE ${filterString}`;
+
+                connection.query(sql, (error, results) => {
+                if (error) throw error;
+                res.json(results);
+            });
+        });
+
+                app.listen(port, () => {
+                    console.log(`API is running at http://localhost:${port}`);
+        });
+
+                process.on("SIGINT", () => {
+                connection.end();
+                process.exit();
+            });
+        })
+        .catch((err) => {
+            console.log("An error occurred: ", err);
+        });
--- source/populateDB.js
+++ source/populateDB.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const Papa = require("papaparse")
+const papa = require("papaparse")
 const util = require("util");
 const createDBConnect = require('./dbConnect');
 
@@ -37,26 +37,100 @@ function populateDatabase() {
     );
 
     const subjectNames = Object.keys(subjects[0]).filter(
-      (header) => header !== "EPISODE" && !=="TITLE"
+      (header) => header !== "EPISODE" && header !=="TITLE"
     );
 
     const db = createDBConnect();
 
     const query = util.promisify(db.query).bind(db);
 
-    const colorPromises = colorNames.map((colorName)) => {
-      const sql = "INSERT INTO COLORS (Color_Name) VALUES (?)";
+    const colorPromises = colorNames.map((colorName) => {
+      const sql = "INSERT INTO Colors (Color_Name) VALUES (?)";
       return query(sql, [colorName])
         .then(() =>
-        console.log(
-          'Inserted color ${colorName} sucessfully into the database'
+          console.log(
+            `Successfully inserted color ${colorName} into the database.`
           )
         )
         .catch((error) => {
-          console.log('Did not insert color ${colorName} into database due to error');
+          console.log(`Failed to insert color ${colorName} into the database.`);
           throw error;
         });
-    }
-}
+    });
 
+    const subjectPromises = subjectNames.map((subjectName) => {
+      const sql = "INSERT INTO Subjects (Subject_Name) VALUES (?)";
+      return query(sql, [subjectName])
+        .then(() =>
+          console.log(
+            `Successfully inserted subject ${subjectName} into the database.`
+          )
+        )
+        .catch((error) => {
+          console.log(
+            `Failed to insert subject ${subjectName} into the database.`
+          );
+          throw error;
+        });
+    });
+
+    const episodePromises = episodes.map(async (episodeData, index) => {
+      const title = episodeData.TITLE;
+      const month = episodeData.DATE.replace(/ "/g, "").split(" ")[0];
+      const season = colors[index].season; // assuming colors and episodes are in the same order
+      const episode = colors[index].episode; // assuming colors and episodes are in the same order
+  
+      const sql =
+        "INSERT INTO Episodes (episode_title, season, episode, month) VALUES (?, ?, ?, ?)";
+      try {
+        const result = await query(sql, [title, season, episode, month]);
+        console.log(`Successfully inserted episode ${title} into the database.`);
+        const episodeId = result.insertId;
+
+        const colorPromises = colorNames.map(async (colorName) => {
+          if (colors[index][colorName] == "1") {
+
+            const sqlColorId = "SELECT Color_Id FROM Colors WHERE Color_Name = ?";
+            const resultColor = await query(sqlColorId, [colorName]);
+            const colorId = resultColor[0].Color_Id;
+
+            const sqlAssociationColor =
+              "INSERT INTO Episode_Color (Episode_Id, Color_Id) VALUES (?, ?)";
+            await query(sqlAssociationColor, [episodeId, colorId]);
+            console.log(
+              `Successfully associated color ${colorName} with episode ${title}.`
+            );
+          }
+        });
+
+        const subjectPromises = subjectNames.map(async (subjectName) => {
+          if (subjects[index][subjectName] == "1") {
+
+            const sqlSubjectId =
+              "SELECT Subject_Id FROM Subjects WHERE Subject_Name = ?";
+            const resultSubject = await query(sqlSubjectId, [subjectName]);
+            const subjectId = resultSubject[0].Subject_Id;
+  
+            const sqlAssociationSubject =
+              "INSERT INTO Episode_Subject (Episode_Id, Subject_Id) VALUES (?, ?)";
+            await query(sqlAssociationSubject, [episodeId, subjectId]);
+            console.log(
+              `Successfully associated subject ${subjectName} with episode ${title}.`
+            );
+          }
+        });
+        await Promise.all([...colorPromises, ...subjectPromises]);
+      } catch (error) {
+        console.log(`Failed to insert episode ${title} into the database.`);
+        throw error;
+      }
+    });
+  
+    Promise.all([...colorPromises, ...subjectPromises, ...episodePromises])
+      .then(() => db.end())
+      .catch((error) =>
+        console.error("Error during database insertions:", error)
+      );
+  }
+  
 module.exports = populateDatabase;
