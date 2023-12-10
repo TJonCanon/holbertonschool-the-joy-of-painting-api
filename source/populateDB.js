f7996b44084140d50e528b108b79a830ab2514de
TJonCanon <taylor.joyner93@me.com>
2023-12-10 15:04:41 -0600

    PopulateDB - open data files

--- source/populateDB.js
+++ source/populateDB.js
@@ -1,22 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+const Papa = require("papaparse")
+const util = require("util");
 const createDBConnect = require('./dbConnect');
 
-async function populateDatabase() {
-  try {
-  
-    const db = createDBConnect();
-
-    
-    data.forEach((row) => {
-      db.query(`INSERT INTO ... VALUES ...`, (error, results, fields) => {
-        if (error) throw error;
-
-      });
-    });
-
-    db.end();
-  } catch (error) {
-    console.error('Error occurred:', error);
-  }
+function populateDatabase() {
+  const colorData = fs.readFileSync(
+    path.join(__dirname, "../datasets/Colors.csv"),
+    "utf-8"
+  );
+  const episodeData = fs.readFileSync(
+    path.join(__dirname, "../datasets/Episodes.csv"),
+    "utf-8"
+  );
+  const subjectData = fs.readFileSync(
+    path.join(__dirname, "../datasets/subjects.csv"),
+    "utf-8"
+  );
 }
 
 module.exports = populateDatabase;
